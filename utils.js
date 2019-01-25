759dadf23a5021786f05b4e492f8a4c131e6963b
KurozeroPB <root@kurozeropb.info>
2019-01-25 11:31:41 +0100

    Send webhook message when anime is uploaded

--- utils.js
+++ utils.js
@@ -1,3 +1,26 @@
+/**
+ * Capitalize the first letter of a string
+ * @returns {String}
+ */
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
-}
+};
+
+/**
+ * Wait x miliseconds
+ * @param {Number} sleep
+ * @returns {Promise<void>}
+ */
+exports.sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+
+/**
+ * Async wrapper for forEach()
+ * @param {Array} a
+ * @param {Function} cb
+ * @returns {Promise<any>}
+ */
+exports.foreachAsync = async (a, cb) => {
+    for (let index = 0; index < a.length; index++) {
+        await cb(a[index], index, a);
+    }
+};
